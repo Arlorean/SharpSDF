74b90012cbe65b68db30195e6bf3f77babb4bad1
Adam Davidson <adam@adam-davidson.com>
2025-02-04 12:11:32 +0000

    Use the compiled shader for the CanvasRenderer in Fable

--- Program.fs
+++ Program.fs
@@ -5,15 +5,15 @@ let shader1 = TestShader.shader
 let shader2 p = Colors.red
 
 let main() =
+    let compiledShader = Interpreter2.compileToInterpreter shader1
 #if FABLE_COMPILER
     // let webGL = new WebGLRenderer.WebGLRenderer("#WebGLRenderer")
     // let shaderSource = ShaderGenerator.makeShader TestShader.shader
     // webGL.RenderSource( shaderSource )
 
     let canvas = new CanvasRenderer.CanvasRenderer("#CanvasRenderer")
-    canvas.Render shader1
+    canvas.Render compiledShader
 #else
-    let compiledShader = Interpreter2.compileToInterpreter shader1
     // let result = compiledShader (HLSL.float2(150,150))
     // printf "%A" result
 
